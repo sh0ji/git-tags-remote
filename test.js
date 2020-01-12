ff6658f907358f09209473561796f74783022f4b
Evan Yamanishi <yamanishi1@gmail.com>
2020-01-12 15:41:13 -0500

    test: update tests

--- test.js
+++ test.js
@@ -2,37 +2,24 @@ import test from 'ava';
 import gtr from '.';
 
 const https = 'https://github.com/sh0ji/focus-rover.git';
-// const ssh = 'git@gitlab.com:wwnorton/platform/epub-build.git';
 const invalid = 'github.com/sh0ji/focus-rover';
 
 test('public https get', async (t) => {
 	const tags = await gtr.get(https);
-	t.is(tags.get('v1.0.0-rc.1'), '48d18c388f4e0b1a963a409bfe3d8fedb3acd50a');
-	t.is(tags.get('v1.0.0-rc.2'), '8e048a0fd9cb668366eef550be445ac761efd667');
+	t.is(tags.get('v1.0.0'), '912af5c2734756b2fa2973a909134775b99c2f7c');
+	t.is(tags.get('v2.0.0'), '7480367b30525107d89aac5ae08cfec991a4e933');
 });
 
 test('public https latest', async (t) => {
 	const latest = await gtr.latest(https);
-	t.is(latest[0], 'v1.0.0-rc.2');
-	t.is(latest[1], '8e048a0fd9cb668366eef550be445ac761efd667');
+	t.is(latest[0], 'v2.0.1');
+	t.is(latest[1], '0b08ebd3a456f12136cfd599401d796c489e82e6');
 });
 
-// test('private ssh get', async (t) => {
-//	 const tags = await gtr.get(ssh);
-//	 t.is(tags.get('v1.0.0-beta.1'), 'cf1f4d2017367cda6bbf78d6624d89e26529d6be');
-//	 t.is(tags.get('v1.0.0-rc.2'), '812e4e8af098a45352b1309d22141d47274994e4');
-// });
-//
-// test('private ssh latest', async (t) => {
-//	 const latest = await gtr.latest(ssh);
-//	 t.is(latest[0], 'v1.0.0-rc.2');
-//	 t.is(latest[1], '812e4e8af098a45352b1309d22141d47274994e4');
-// });
-
 test('invalid url', async (t) => {
-	const error = await t.throws(gtr.get(invalid));
+	const error = await t.throwsAsync(gtr.get(invalid));
 	t.is(
-		error,
+		error.message,
 		`fatal: '${invalid}' does not appear to be a git repository\n`
 		+ 'fatal: Could not read from remote repository.\n\n'
 		+ 'Please make sure you have the correct access rights\n'
