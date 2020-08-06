d4174cec145974897cf4a0a038c797f65a8959dc
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-06 11:42:31 -0400

    refactor: rewrite in typescript

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { valid, compare } from 'semver';
+import { exec } from 'child_process';
+
+const lsRemoteTags = (repo: string): Promise<string> => new Promise(
+	(resolve, reject) => {
+		exec(`git ls-remote --tags ${repo}`, (_, stdout, stderr) => {
+			if (stderr) reject(new Error(stderr));
+			resolve(stdout.toString().trim());
+		});
+	},
+);
+
+const parseTags = (tags: string): Map<string, string> => {
+	const tagMap = new Map();
+	tags.split('\n')
+		.forEach((str) => {
+			const ref = str.split(/\t/);
+			tagMap.set(ref[1].split('/')[2].replace(/\^\{\}$/, ''), ref[0]);
+		});
+	return new Map(
+		[...tagMap.entries()]
+			.filter((arr) => valid(arr[0]))
+			.sort((a, b) => compare(a[0], b[0]))
+			.reverse(),
+	);
+};
+
+export const get = async (repo: string): Promise<Map<string, string>> => {
+	const tags = await lsRemoteTags(repo);
+	return parseTags(tags);
+};
+
+export const latest = async (repo: string): Promise<Map<string, string>> => {
+	const tags = await get(repo);
+	return tags.entries().next().value;
+};
+
+module.exports = { get, latest };
